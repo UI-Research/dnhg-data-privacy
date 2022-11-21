8dbf25b593a8cea049b8c038c52d6be4137dc7a7
Joshua Snoke <snoke4@gmail.com>
2022-11-21 09:04:53 -0500

    Adding ground truth data files

--- generate_ground_truth_data.R
+++ generate_ground_truth_data.R
@@ -7,6 +7,10 @@
 # OUTPUTS:
 #     output_data: microdata
 
+library(tidyverse)  # for data wrangling and visualization
+library(readxl)     # for importing excel (data)
+
+
 generate_micro_data = function(input_data){
   output_data = tibble(geographic_school_district = rep(input_data$geographic_school_district, times = input_data$children_5_17),
                        poverty = rep(NA, (sum(input_data$children_5_17))),
@@ -35,4 +39,66 @@ generate_micro_data = function(input_data){
   }
   return(output_data)
 }
+
+## load in data
+
+# New Mexico Data
+data_nm <- read_excel("data/data/nhgis_district_data_var.xlsx") %>%
+  filter(state == "New Mexico") %>%
+  dplyr::select(
+    geographic_school_district,
+    children_5_17,
+    poverty,
+    linguistically_isolated,
+    children_disability,
+    vulnerable_job,
+    single_parent,
+    crowded_conditions, 
+    no_computer_internet
+  )
+
+# Pennsylvania Data
+data_pa <- read_excel("data/data/nhgis_district_data_var.xlsx") %>%
+  filter(state == "Pennsylvania") %>%
+  dplyr::select(
+    geographic_school_district,
+    children_5_17,
+    poverty,
+    linguistically_isolated,
+    children_disability,
+    vulnerable_job,
+    single_parent,
+    crowded_conditions, 
+    no_computer_internet
+  ) %>%
+  head(-1) # Empty district
+
+
+## generate microdata (one row per child)
+nm_simulated_ground_truth = generate_micro_data(data_nm)
+
+pa_simulated_ground_truth = generate_micro_data(data_pa)
+
+## generate proportion data (same original format)
+nm_simulated_ground_truth_tabular = nm_simulated_ground_truth %>%
+  group_by(geographic_school_district) %>%
+  mutate(children_5_17 = n(), .before = poverty) %>%
+  summarize_all(mean)
+
+pa_simulated_ground_truth_tabular = pa_simulated_ground_truth %>%
+  group_by(geographic_school_district) %>%
+  mutate(children_5_17 = n(), .before = poverty) %>%
+  summarize_all(mean)
+
+## save
+write_csv(nm_simulated_ground_truth, 'data/simulated_ground_truth/nm_simulated_ground_truth.csv')
+
+write_csv(pa_simulated_ground_truth, 'data/simulated_ground_truth/pa_simulated_ground_truth.csv')
+
+write_csv(nm_simulated_ground_truth_tabular, 'data/simulated_ground_truth/nm_simulated_ground_truth_tabular.csv')
+
+write_csv(pa_simulated_ground_truth_tabular, 'data/simulated_ground_truth/pa_simulated_ground_truth_tabular.csv')
+
+
+
 
